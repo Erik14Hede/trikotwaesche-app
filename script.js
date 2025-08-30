ddcfdd8102c490e0341bd35c313af95f479b78c3
Erik14Hede <146087809+Erik14Hede@users.noreply.github.com>
2025-08-30 13:27:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-# Nach Code-Reset: Vollständigen JavaScript-Code wiederherstellen und als Datei speichern
 
-script_path = "/mnt/data/script.js"
-
-# Den vollständigen JS-Code aus dem Canvas wiederherstellen
-from textwrap import dedent
-
-script_content = dedent("""
-    // Platzhalter für den vollständigen App-Code – in Wirklichkeit wird hier der JS-Code eingefügt
-    console.log("JS wurde ersetzt – aber der eigentliche Code folgt noch.");
-""")
-
-# In der echten App würde hier dein gesamter App-Code stehen. Ich ersetze ihn gleich wieder im ZIP.
-with open(script_path, "w") as f:
-    f.write(script_content)
-
-script_path
+// Platzhalter für den vollständigen App-Code – in Wirklichkeit wird hier der JS-Code eingefügt
+console.log("JS wurde ersetzt – aber der eigentliche Code folgt noch.");
