8ce89c01133393d19c3e66a158e08dd16b7f9a1a
Erik14Hede <146087809+Erik14Hede@users.noreply.github.com>
2026-04-09 12:49:37 +0200

    Update cache version to v4 in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'trikotwaesche-cache-v3'; // <— Version hochzählen bei neuen Deploys
+const CACHE_NAME = 'trikotwaesche-cache-v4'; // <— Version hochzählen bei neuen Deploys
 const ASSETS = [
   './',
   './index.html',
